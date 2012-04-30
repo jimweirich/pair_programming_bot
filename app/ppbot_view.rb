5b7b445075226ec7df9ee0938fd9cae910332f98
Jim Weirich <jim.weirich@gmail.com>
2012-04-30 14:07:56 -0400

    Initial working version

---
+++ app/ppbot_view.rb
@@ -0,0 +1,42 @@
+class PPBotView < UIView
+  def init
+    @touches = 0
+    super
+  end
+
+  def drawRect(rect)
+    if @moved
+      bgcolor = begin
+        red, green, blue = rand(100), rand(100), rand(100)
+        UIColor.colorWithRed(red/100.0, green:green/100.0, blue:blue/100.0, alpha:1.0)
+      end
+      text = "ZOMG!"
+    else
+      bgcolor = UIColor.blackColor
+      text = @touches != 0 ? "Touched #{@touches} times!" : "Hello RubyMotion!"
+    end
+
+    bgcolor.set
+    UIBezierPath.bezierPathWithRect(frame).fill
+
+    font = UIFont.systemFontOfSize(24)
+    UIColor.whiteColor.set
+    text.drawAtPoint(CGPoint.new(10, 20), withFont:font)
+  end
+
+  def touchesMoved(touches, withEvent:event)
+    @moved = true
+    setNeedsDisplay
+  end
+
+  def touchesEnded(touches, withEvent:event)
+    @moved = false
+    bump
+  end
+
+  def bump
+    @touches += 1
+    setNeedsDisplay
+  end
+
+end
